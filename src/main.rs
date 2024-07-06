1f662c36989005e3cd2705728b78d96e6f50b7a4
Swarup Suryawanshi <suryawanshiswarup@gmail.com>
2024-07-06 19:20:37 +0530

    for loop

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 fn main () {
     let mut counter = 0;
+
+    println!("LOOP");
     let result = loop {
         counter += 1;
 
@@ -15,11 +17,24 @@ fn main () {
     // while loop
     let mut number = 3;
 
+    println!("\nWHILE LOOP");
     while number != 0 {
         println!("{}!", number);
 
         number -= 1;
     }
 
-    println!("LIFTOFF!!!")
+    println!("LIFTOFF!!!");
+
+    // for loop
+    let a = [10, 20, 30, 40, 50];
+
+    println!("\nFOR LOOP");
+    for element in a.iter() {
+        println!("The value is: {}", element);
+    }
+
+    for number in 1..4 {
+        println!("{}!", number)
+    }
 }
