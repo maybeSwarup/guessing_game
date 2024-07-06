3c9948eb36c77793e32bd9c00e49e75e9303abad
Swarup Suryawanshi <suryawanshiswarup@gmail.com>
2024-07-06 19:10:16 +0530

    functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main () {
-    
+    let sum = add_integers(11, 22);
+    println!("sum: {}", sum);
+}
+
+fn add_integers (x: i32, y: i32) -> i32 {
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
+    x + y
 }
