af888876815af70ac40cb23873253f6030573dd2
Swarup Suryawanshi <suryawanshiswarup@gmail.com>
2024-07-06 20:18:41 +0530

    shadow copy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main () {
-    
+    let x = 5;
+    let y = x; // x is copied to y
+    println!("x: {}, y: {}", x, y);
+
+    let s1 = String::from("hello");
+    let s2 = s1; // shadow copy, bad practice!
+    let s2 = s2.clone();
+
+    println!("{}, world!", s2);
 }
