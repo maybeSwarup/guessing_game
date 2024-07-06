d8c860666622050562024c4fb29e844b38425ba2
Swarup Suryawanshi <suryawanshiswarup@gmail.com>
2024-07-06 11:22:08 +0530

    init

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,3 @@
-use rand::Rng;
-use std::cmp::Ordering;
-use std::io;
-use colored::*;
-
-fn main() {
-    println!("Guess the number!");
-
-    let secret_number = rand::thread_rng().gen_range(1, 101);
-
-    println!("The secret number is: {}", secret_number);
-
-    loop {
-        println!("Please input your guess.");
-
-        let mut guess = String::new();
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
-
-        println!("You guessed: {}", guess);
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("{}", "Too small!".red()),
-            Ordering::Greater => println!("{}", "Too big!".red()),
-            Ordering::Equal => {
-                println!("{}", "You win!".green());
-                break;
-            },
-        }
-    }
+fn main () {
+    
 }
