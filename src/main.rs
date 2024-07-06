140fb597ed34c98bae0f111ba2af62e610127226
Swarup Suryawanshi <suryawanshiswarup@gmail.com>
2024-07-06 19:16:51 +0530

    while loop

--- src/main.rs
+++ src/main.rs
@@ -11,4 +11,15 @@ fn main () {
     };
 
     println!("The result is: {}", result);
+
+    // while loop
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{}!", number);
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!")
 }
