9ea4589eacf140b52a48a5d0578866defe3b4652
Swarup Suryawanshi <suryawanshiswarup@gmail.com>
2024-07-06 19:14:48 +0530

    loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main () {
-    
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+
+        println!("counter is {}", counter);
+
+        if counter == 10 {
+            break counter;
+        }
+    };
+
+    println!("The result is: {}", result);
 }
