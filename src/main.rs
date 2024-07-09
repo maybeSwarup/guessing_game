ee65e0ef0257c54e18f16a31eccf840bbf3bf77e
Swarup Suryawanshi <suryawanshiswarup@gmail.com>
2024-07-09 11:20:41 +0530

    mutable reference

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 fn main () {
-    let s1 = String::from("hello");
-    let len = calculate_length(&s1);
-    println!("The length of '{}' is {}.", s1, len);
+    let mut s1 = String::from("hello");
+    change(&mut s1);
+    println!("s1: {}", s1); // prints "hello, world"
 }
 
-fn calculate_length (s: &String) -> usize {
-    s.len()
+fn change (some_string: &mut String) {
+    some_string.push_str(", world")
 }
